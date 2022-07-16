f61c3290dd1012f71cfcbc0b11f4a549f9ebc52f
Mrtest626 <109091058+Mrtest626@users.noreply.github.com>
2022-07-16 14:41:32 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -22,7 +22,7 @@ async def save_group(bot, message):
         if message.chat.id in temp.BANNED_CHATS:
             # Inspired from a boat of a banana tree
             buttons = [[
-                InlineKeyboardButton('𝗢𝘄𝗻𝗲𝗿', url=f'https://t.me/{SUPPORT_CHAT}')
+                InlineKeyboardButton('𝗢𝘄𝗻𝗲𝗿', url=f'https://t.me/Anonymousbro36')
             ]]
             reply_markup=InlineKeyboardMarkup(buttons)
             k = await message.reply(
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [
             [
-                InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/mh_world')
+                InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/+dcGYHE8Qng00OWFl')
             ],
             [
                 InlineKeyboardButton('♻️ HΞLᎮ ♻️', url=f"https://t.me/{temp.U_NAME}?start=help")
@@ -69,7 +69,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('𝗢𝘄𝗻𝗲𝗿', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('𝗢𝘄𝗻𝗲𝗿', url=f'https://t.me/Anonymousbro36')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
@@ -107,7 +107,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Succesfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('𝗢𝘄𝗻𝗲𝗿', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('𝗢𝘄𝗻𝗲𝗿', url=f'https://t.me/Anonymousbro36')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
